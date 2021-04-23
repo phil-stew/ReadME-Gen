a24aab5fa6b19faed84fa34d4619a7a27b6838b7
Philly <Phil.stew@live.ca>
2021-04-23 00:27:47 -0400

    updated readme code gen

---
+++ assets/js/ReadMe.md
@@ -0,0 +1,28 @@
+This is a ReadME for dhsfjkdlf application created by dfsd
+
+Description:
+dfdfd
+
+Installation:
+dfdfd
+
+Usage: 
+dddfd
+
+Contributing: 
+dfdf
+
+Tests:
+dfdf
+
+Questions:
+ 
+Feel free to get more infomation by contacting me on github or email
+
+GitHub: fdf    Email: dfdf
+
+
+
+
+
+
--- assets/js/index.js
+++ assets/js/index.js
@@ -1,12 +1,17 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
+const fs = require('fs');
+const util = require('util');
+
+const writeFileAsync = util.promisify(fs.writeFile);
 
 // TODO: Create an array of questions for user input
-const questions = [ 
+const writeReadMe = () => {
+    return inquirer.prompt([
     {
         type: 'input',
         message: 'What is your name?',
-        name: 'name',
+        name: 'uname',
     },
     {
         type: 'input',
@@ -18,71 +23,99 @@ const questions = [
         message: 'What is this application used for?, How do i test it?',
         name: 'usage',
     },
-
     {
         type: 'input',
         message: 'How do you install this application?',
-        name: 'app',
+        name: 'install',
     },
     {
         type: 'input',
         message: 'Why did you build the application? What does it do?',
-        name: 'Description',
+        name: 'description',
     },
-
     {
         type: 'input',
         message: 'Who helped build this application?',
         name: 'contributors',
     },
-
+    {
+        type: 'input',
+        message: 'What does your test prove or solve?',
+        name: 'test',
+    },
     {
         type: 'input',
         message: 'What is what is ur GitHub URL?',
         name: 'github',
     },
-
     {
         type: 'input',
         message: 'What is your email?',
         name: 'email',
-    },
+    }
+ ]);
+};
 
-    
+const genReadMe = (response) => 
+`This is a ReadME for ${response.uname} application created by ${response.projectname}
+
+Description:
+${response.description}
+
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [Credits](#credits)
+- [License](#license)
+- [Questions:](#Questions:)
+- [Contributing](#Contributing)
+- [Tests](#Tests)
+
+##Installation:
+${response.install}
+
+##Usage: 
+${response.usage}
+
+##Contributing: 
+${response.contributors}
+
+##Tests:
+${response.test}
+
+##Questions:
+ 
+Feel free to get more infomation by contacting me on github or email
+
+GitHub: ${response.github}    Email: ${response.email}
+
+
+
+
+
+
+`
+
+
+
+
+
+
+const init = () => {
+    writeReadMe()
+      .then((answers) => writeFileAsync('README.md', genReadMe(answers)))
+      .then(() => console.log('Successfully wrote to README.md'))
+      .catch((err) => console.error(err));
+  };
   
 
-];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
 
+init();
 // TODO: Create a function to initialize app
-function init() {}
+
 
 // Function call to initialize app
-init();
 
-
-inquirer
-  .prompt([
-    {
-      type: 'input',
-      message: 'What is your user name?',
-      name: 'username',
-    },
-    {
-      type: 'password',
-      message: 'What is your password?',
-      name: 'password',
-    },
-    {
-      type: 'password',
-      message: 'Re-enter password to confirm:',
-      name: 'confirm',
-    },
-  ])
-  .then((response) =>
-    response.confirm === response.password
-      ? console.log('Success!')
-      : console.log('You forgot your password already?!')
-  );
+    
